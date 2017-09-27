be69f8e32b6072cc1af0d3855d0438079b83babd
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-27 13:49:09 -0400

    Updated from Brython Server: 9/27/2017 1:49:09 PM

--- favorites.py
+++ favorites.py
@@ -2,7 +2,7 @@
 #9/27/17
 #favorites.py - print out word
 
-word = input('Enter your favorite word')
+word = input('Enter your favorite word: ')
 num = int(input('Enter your favorite number: '))
 
 for i in range(0,num):
