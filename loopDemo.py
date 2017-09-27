b022fc9dc332bd703919e959c7ba69260f5f6011
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-27 13:46:27 -0400

    Updated from Brython Server: 9/27/2017 1:46:26 PM

--- loopDemo.py
+++ loopDemo.py
@@ -1,17 +1,51 @@
 #Pedro Gallino
 #9/27/17
-#loopDemo.py - First loop
+#loopDemo.py - loops for anf while
 
 """
 #print I love computer science 5 times
 for i in range(0,5):
     print('I love computer science')
-"""
 
+i = 1
+while i <= 5:
+    print('I love computer science')
+    i = i+1
+"""
+"""
 #print numbers from 1 to 20
 for i in range(1,21):
      print(i)
 
+i=1
+while i <= 20:
+    print(i)
+    i = i+1
+"""
+"""
+#print out the odd numbers from 13-27
+for i in range(13,28,2): #(starting number, ending number + 1, what it goes up by - can't be decimel) 
+    print(i)
 
+i = 13
+while i<= 27:
+    print(i)
+    i = i+2
+"""
+"""
+total = 0
+for i in range(1,6):
+    total = total+i
+print(total)
 
+total = 0
+i = 1
+while 1<=5:
+    total += i #SAME THING AS: total = total+i
+    i+= 1  #SAME THING AS: i = i+1
+print(total)
+"""   
+    
+    
+    
 
