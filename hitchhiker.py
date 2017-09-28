7a154519d2e6c7a3932d1a87df850c0c05379f23
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-28 14:38:50 -0400

    Updated from Brython Server: 9/28/2017 2:38:49 PM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,1 +1,11 @@
+#Pedro Gallino
+#9/28/17
+#hitchhiker.py - awnsers 42
 
+
+while True:
+    question = input('Ask a question or type "quit": ')
+    if question == 'quit' or question == 'Quit':
+        break
+    print('42')
+    
