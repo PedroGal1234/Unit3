3031fbbc0081b5fe4ef4a56f2ab032a942d40923
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-28 14:56:14 -0400

    Updated from Brython Server: 9/28/2017 2:56:13 PM

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,1 +1,20 @@
+#Pedro Gallino
+#9/28/17
+#numberGuessingGame.py - is a number guessing game
 
+from random import randint
+
+num = randint(1,100)
+runs = 0
+
+while True:
+    runs += 1
+    guess = int(input('Guess a number'))
+    if guess > num:
+        print('Guess is to high')
+    if guess < num:
+        print('Guess is to low')
+    if guess == num:
+        print('The number is', num)
+        break
+print('You got it in',runs,'tries')
