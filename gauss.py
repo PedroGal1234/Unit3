a74031b600823486bcca69cd8b9adb734a910570
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-28 14:27:28 -0400

    Updated from Brython Server: 9/28/2017 2:27:17 PM

--- gauss.py
+++ gauss.py
@@ -2,7 +2,10 @@
 #9/28/17
 #gauss.py - adds numbers form 1 to 100 up
 
+max = int(input('Enter a Number: '))
+difference = int(input('Enter difference'))
 num = 0
-for i in range(1,101):
+
+for i in range(1,(max+1),difference):
     num=num+i
 print(num)
