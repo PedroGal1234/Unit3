43f1cc6776fe37f71d0b359905671dc2f389a17b
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-29 14:45:13 -0400

    Updated from Brython Server: 9/29/2017 2:45:13 PM

--- fri13.py
+++ fri13.py
@@ -2,4 +2,12 @@
 #9/29/17
 #fir13.py - tells when the next firday the 13th will be for the next 10 years
 
+from datetime import date
+from calendar import weekday
 
+year = date.today().year
+month = date.today().month 
+date = date.today().day 
+
+while True:
+    if weekday(year,month,13)
