c2a2cce5732a5d12757c0f570802b1b1dde17f82
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-29 14:50:35 -0400

    Updated from Brython Server: 9/29/2017 2:50:35 PM

--- fri13.py
+++ fri13.py
@@ -13,9 +13,9 @@ i = 0
 while True:
     if weekday(year,month+i,13) == 4:
         print(month,'/',13,'/',year)
+        repeats = repeats + 1
     if month == 12:
         month = 0
-        repeats = repeats + 1
         year = year +1
     month = month + 1
     if repeats == 10:
