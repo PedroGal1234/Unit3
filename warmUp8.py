f0644b09e770560a4852df4b68b444ba8c707a26
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-02 14:18:35 -0400

    Updated from Brython Server: 10/2/2017 2:18:34 PM

--- warmUp8.py
+++ warmUp8.py
@@ -1,1 +1,9 @@
+#Pedro Gallino
+#10/2/17
+#warmUp8.py - find sum of all positive integers less than 100,000 that are divisible by 3, 1o, and 17
 
+sum = 0
+for i in range(1,100001):
+    if i%3 == 0 and i%10 == 0 and i%17 == 0:
+        sum = sum+i
+print(sum)
