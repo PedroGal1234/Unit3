f31f538bb96f71cfd9c5f79771912e7b4ea28b6d
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-04 13:39:47 -0400

    Updated from Brython Server: 10/4/2017 1:39:46 PM

--- betterAditionGameDemo.py
+++ betterAditionGameDemo.py
@@ -1,1 +1,20 @@
+#Pedro Gallino
+#10/4/17
+#betterAditionGameDemo.py - ask adition till user gets 5
+
+from random import randint
+
+numCorrect = 0
+
+while numCorrect < 5:
+    num1 = randint(-10,10)
+    num2 = randint(-10,10)
+    question = 'What is '+ str(num1) + '+' + str(num2) + '?'
+    awnser = int(input(question))
+    if num1 + num2 == awnser:
+        print('Correct!')
+        numCorrect += 1
+    else:
+        print('The answer was', num1+num2)
+print("Congratiolations! You Win!")
 
