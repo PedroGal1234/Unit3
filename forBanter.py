817495e955867a3368313ad720815660bbf922dc
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-04 13:58:10 -0400

    Updated from Brython Server: 10/4/2017 1:58:09 PM

--- forBanter.py
+++ forBanter.py
@@ -1,1 +1,36 @@
+#Pedro Gallino
+#10/2/17
+#forBanter.py - Fooling Around
+
+
+red = Color(0xff0000,1)
+green = Color(0x00ff00,1)
+blue = Color(0x000ff,1)
+black = Color(0x00000,1)
+
+redOutline = LineStyle(2,red)
+greenOutline = LineStyle(2,black)
+blueOutline = LineStyle(2,blue)
+blackOutline = LineStyle(2,black)
+
+redRectangle = RectangleAsset(100,100,blackOutline,red)
+Sprite(redRectangle,(175,100))
+
+move = input('Move with w, a, s, d,')
+vertical = 400
+horizontal = 400
+
+while True:
+    if move == 'w': 
+        vertical = +100
+        Sprite(redRectangle,(vertical,horizontal))
+    if move == 'lol':
+        break
+
+
+App().run()
+
+
+
+
 
