5ec33ace55e6e3ce8e1914f3ff7fe1159facbbb1
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-04 13:45:03 -0400

    Updated from Brython Server: 10/4/2017 1:45:03 PM

--- warmUp9.py
+++ warmUp9.py
@@ -1,1 +1,12 @@
+#Pedro Gallino
+#10/4/17
+#warmpUp9.py - print out word with capital vowels
+
+word = input('Enter a word')
+
+for ch in word:
+    if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+        print(ch.upper())
+    else:
+        print(ch)
 
