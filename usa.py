dbf97435edd29d5d9ed0d2bd686b9511baf40630
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-12 14:38:11 -0400

    Updated from Brython Server: 10/12/2017 2:38:10 PM

--- usa.py
+++ usa.py
@@ -39,44 +39,13 @@ for longLines in range (1,8):
 blueRectangle = RectangleAsset(300,175,blueOutline,blue)
 Sprite(blueRectangle,(100,25))
 
-horizontalRow1 = 0
-horizontalRow2 = 0
-horizontalRow3 = 0
-horizontalRow4 = 0
-horizontalRow5 = 0
-horizontalRow6 = 0
-horizontalRow7 = 0
-horizontalRow8 = 0
-horizontalRow9 = 0
-for i in range(1,51):
-    star = PolygonAsset([(0,5.5),(5,5),(6.5,0),(9,5),(14,5.5),(10.5,9),(12,14),(6.5,11.5),(2,14),(3.5,9)],whiteOutline,white)
-    if i <= 6:
-        Sprite(star,(110+horizontalRow1,28))
-        horizontalRow1 = horizontalRow1 + 53
-    elif i <= 11:
-        Sprite(star,(135+horizontalRow2,46))
-        horizontalRow2 = horizontalRow2 + 53
-    elif i <= 17:
-        Sprite(star,(110+horizontalRow3,64))
-        horizontalRow3 = horizontalRow3 + 53
-    elif i <= 22:
-        Sprite(star,(135+horizontalRow4,82))
-        horizontalRow4 = horizontalRow4 + 53
-    elif i <= 28:
-        Sprite(star,(110+horizontalRow5,100))
-        horizontalRow5 = horizontalRow5 + 53
-    elif i <= 33:
-        Sprite(star,(135+horizontalRow6,118))
-        horizontalRow6 = horizontalRow6 + 53
-    elif i <= 39:
-        Sprite(star,(110+horizontalRow7,136))
-        horizontalRow7 = horizontalRow7 + 53
-    elif i <= 44:
-        Sprite(star,(135+horizontalRow8,154))
-        horizontalRow8 = horizontalRow8 + 53
-    elif i <= 60:
-        Sprite(star,(110+horizontalRow9,172))
-        horizontalRow9 = horizontalRow9 + 53
+star = PolygonAsset([(0,5.5),(5,5),(6.5,0),(9,5),(14,5.5),(10.5,9),(12,14),(6.5,11.5),(2,14),(3.5,9)],whiteOutline,white)
+for j in range(0,5):
+    for i in range(0,6):
+        Sprite(star,(110+53*i,28+36*j))
+for j in range(0,4):
+    for i in range(0,5):
+        Sprite(star,(135+53*i,46+36*j))
 
 App().run()
     
