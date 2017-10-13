5fca6f318c875fa897951c48919eb9e5a22b6a4e
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-13 14:24:23 -0400

    Updated from Brython Server: 10/13/2017 2:24:21 PM

--- quiz3.py
+++ quiz3.py
@@ -1,1 +1,22 @@
+#Pedro Gallino
+#10/13/17
+#My thrid proggraming quiz on loops
 
+for i in range(-5,6):
+    print(i)
+
+k = 18
+while k<=32:
+    print(k)
+    k += 2
+
+total = 0
+for y in range(13,332,2):
+    total = total + y
+
+print(total)
+
+while True:
+    word = input('Enter a Word: ')
+    if 'z' in word:
+        break
